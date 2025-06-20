d7e9c1fb54ca901f411e9f83ec15e0250468b1e7
MD. SHIHAB SHAHRIAR <shahriar@MDs-MacBook-Air-2.local>
2025-06-21 00:36:40 +0600

    database added

--- library_management/settings.py
+++ library_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from pathlib import Path
@@ -91,15 +91,23 @@ WSGI_APPLICATION = 'library_management.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': env("DB_NAME"),
+#        'USER': env("DB_USER"),
+#        'PASSWORD': env("DB_PASSWORD"),
+#        'HOST': env("DB_HOST"),
+#        'PORT': env("DB_PORT"),
+#    }
+# }
+
+
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': env("DB_NAME"),
-       'USER': env("DB_USER"),
-       'PASSWORD': env("DB_PASSWORD"),
-       'HOST': env("DB_HOST"),
-       'PORT': env("DB_PORT"),
-   }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://librarymanagement_3myr_user:MYqDZfMET1F77w80nN0R3rjLBMzWWyhx@dpg-d1aqjb6uk2gs7392bp30-a.oregon-postgres.render.com/librarymanagement_3myr',
+    )
 }
 
 
