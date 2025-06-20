202fd3d472a6d1be2e1588d69c4dec410f45c907
MD. SHIHAB SHAHRIAR <shahriar@MDs-MacBook-Air-2.local>
2025-06-21 01:11:12 +0600

    urls.py changed

--- library_management/urls.py
+++ library_management/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('library/', include('library.urls')),    
+    path('', include('library.urls')),    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
